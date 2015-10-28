37349d355329a3dd1116d799d406d7aae9f0caae
mcapizzi <mcapizzi@email.arizona>
2015-10-28 12:41:40 -0700

    domain methods complete

--- Domain.py
+++ Domain.py
@@ -4,18 +4,21 @@ import numpy as np
 
 class Domain:
 
-    def __init__(self, listOfElements):
-        self.listOfElements = listOfElements
-        self.sizeOfDomain = self.domainMatrix.shape[0]
+    #listOfElements = ["john", "chris", "tom"]
+    #listOfPredictes = [("is_mathematician", ["john", "chris"])]
+    def __init__(self, listOfElements, listOfUnaryPredicates = []):
+        self.elements = listOfElements
+        self.predicates = listOfUnaryPredicates
+        self.sizeOfDomain = len(self.elements)
         self.domainDictionary = {}
-        self.domainMatrix = np.zeros((self.sizeOfDomain, self.sizeOfDomain))
-
+        self.domainMatrix = np.zeros((self.sizeOfDomain, self.sizeOfDomain))        #each row is a one-hot
+        self.predicateDictionary = {}
 
     #build domain and lookup dictionary
     def buildDomain(self):
         for elem in range(self.sizeOfDomain):
             #add to lookup dictionary
-            self.domainDictionary[self.listOfElements[elem]] = elem
+            self.domainDictionary[self.elements[elem]] = elem
             #build one-hot vector
             oneHot = np.zeros(self.sizeOfDomain)
             oneHot[elem] = 1
@@ -23,15 +26,22 @@ class Domain:
             self.domainMatrix[elem] = oneHot
 
 
+    # #build predicates and lookup dictionary
+    # def buildPredicates(self):
+    #     for pred in range(len(self.predicates)):
+    #         #build
+
     #add an element to domain
     def addToDomain(self, element):
 
         #add to self.listOfElements
-        self.listOfElements.append(element)
+        self.elements.append(element)
+        #update size of domain
+        self.sizeOfDomain += 1
         #add to self.domainDictionary
-        self.domainDictionary[len(self.listOfElements) - 1] = len(self.listOfElements) - 1
+        self.domainDictionary[element] = self.sizeOfDomain - 1
         #add to self.domainMatrix
-        np.insert(self.domainMatrix, self.domainMatrix.shape[1], 0, 1)      #add a column of zeros
-        np.insert(self.domainMatrix, self.domainMatrix.shape[0], 0, 0)      #add a row of zeros
-        self.domainMatrix[self.sizeOfDomain][self.sizeOfDomain] = 1         #update one-hot vector
+        self.domainMatrix = np.insert(self.domainMatrix, self.domainMatrix.shape[1], 0, 1)      #add a column of zeros
+        self.domainMatrix = np.insert(self.domainMatrix, self.domainMatrix.shape[0], 0, 0)      #add a row of zeros
+        self.domainMatrix[self.domainMatrix.shape[0] - 1][self.domainMatrix.shape[1] - 1] = 1         #update one-hot vector
 
