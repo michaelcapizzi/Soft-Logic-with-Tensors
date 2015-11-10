3a0a335637bf5d044d05d8dc25328ab6d20c2e0f
mcapizzi <mcapizzi@email.arizona>
2015-11-10 16:02:20 -0700

    began building embeddings class

--- Embedding.py
+++ Embedding.py
@@ -7,15 +7,23 @@ class Embedding:
     #get the word2vec embeddings to be used
         #filePath = path to default w2v's
 
-    def __init__(self, trainingSentences, filePath="/home/mcapizzi/Google_Drive_Arizona/Programming/word2Vec/GoogleNews-vectors-negative300.bin.gz"):
+    def __init__(self, filePath="/home/mcapizzi/Google_Drive_Arizona/Programming/word2Vec/GoogleNews-vectors-negative300.bin.gz"):
         self.W2Vpath = filePath
-        self.embeddingModel = gensim.models.word2vec.Word2Vec(sentences=None, size=100, window=8, min_count=1, workers=4, negative=5)
-        self.trainingSentences = trainingSentences
+        self.embeddingModel = None
+        self.trainingSentences = []
         self.total_words = len(list(itertools.chain(*self.trainingSentences)))
 
 
+    def addTrainingSentences(self, listOfSentences):
+        self.trainingSentences = listOfSentences
+
+
     def useDefault(self):
-        self.embeddingModel.load_word2vec_format(fname=self.W2Vpath, binary=True, norm_only=True)
+        self.embeddingModel = gensim.models.Word2Vec.load_word2vec_format(fname=self.W2Vpath, binary=True, norm_only=True)
+
+
+    def buildModel(self, listOfSentences, size, window, min_count, workers, negative):
+        self.embeddingModel = gensim.models.word2vec.Word2Vec(sentences=listOfSentences, size=size, window=window, min_count=min_count, workers=workers, negative=negative)
 
 
     def train(self, listOfSentences):
