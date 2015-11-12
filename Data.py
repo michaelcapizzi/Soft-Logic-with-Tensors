9f8ade6e2f587b1957cd98815e8fd19f9db4153a
mcapizzi <mcapizzi@email.arizona>
2015-11-12 13:36:01 -0700

    added Data file

---
+++ Data.py
@@ -0,0 +1,19 @@
+__author__ = 'mcapizzi'
+
+#import nltk as nl
+
+class Data:
+
+    def __init__(self, textFile):
+        self.file = textFile
+
+#http://www.nltk.org/_modules/nltk/tokenize.html
+
+#use corpus reader
+#http://www.nltk.org/_modules/nltk/corpus/reader/plaintext.html
+
+#figure out how to do dependency parse
+#with stanford parser
+#http://www.nltk.org/_modules/nltk/parse/stanford.html
+
+
--- Embedding.py
+++ Embedding.py
@@ -13,19 +13,31 @@ class Embedding:
         self.trainingSentences = []
         self.total_words = len(list(itertools.chain(*self.trainingSentences)))
 
-
-    def addTrainingSentences(self, listOfSentences):
-        self.trainingSentences = listOfSentences
-
-
+#use GoogleNews vectors
     def useDefault(self):
         self.embeddingModel = models.Word2Vec.load_word2vec_format(fname=self.W2Vpath, binary=True, norm_only=True)
 
 
-    def buildModel(self, listOfSentences, size, window, min_count, workers, negative):
-        self.embeddingModel = models.word2vec.Word2Vec(sentences=listOfSentences, size=size, window=window, min_count=min_count, workers=workers, negative=negative)
+#train new vectors
+
+    #add tokenized sentences
+    def addTrainingSentences(self, listOfSentences):
+        self.trainingSentences = listOfSentences
 
 
-    def train(self, listOfSentences):
-        #do I have to build_vocab?
-        self.embeddingModel.train(sentences=listOfSentences, total_words=self.total_words)
+    #build a model with pre-loaded sentences
+    def buildModel(self, window, min_count, workers, negative, size=100, ):
+        self.embeddingModel = models.word2vec.Word2Vec(sentences=self.trainingSentences, size=size, window=window, min_count=min_count, workers=workers, negative=negative)
+        #when all training is complete - saves memory
+        self.embeddingModel.init_sims(replace=True)
+
+    #train line by line in stream
+    def trainLineByLine(self, stream, window, min_count, workers, negative, size=100):
+        #intialize model
+        self.embeddingModel = models.word2vec.Word2Vec(sentences=None, size=size, window=window, min_count=min_count, workers=workers, negative=negative)
+
+        #TODO confirm this is done correctly using the same sentences both times (for build_vocab and train)
+        for line in stream:
+            self.embeddingModel.build_vocab(line.split(" "))        #TODO confirm how input is coming in
+            self.embeddingModel.train(line.split(" "))
+
