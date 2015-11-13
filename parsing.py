b1bd97f892ca8fc07a26feab752b40c13df9989b
mcapizzi <mcapizzi@email.arizona>
2015-11-13 11:02:37 -0700

    added capacity for SENNA

--- parsing.py
+++ parsing.py
@@ -1,8 +1,13 @@
+# from practnlptools import tools
+
 __author__ = 'mcapizzi'
 
 
 import StanfordDependencies
 from nltk.parse import stanford
+
+############################################
+#using Stanford parser and dependency wrapper
 
 #stanford parser
 parser = stanford.StanfordParser("/home/mcapizzi/Github/Semantics/stanford-parser-full-2014-08-27/stanford-parser.jar", "/home/mcapizzi/Github/Semantics/stanford-parser-full-2014-08-27/stanford-parser-3.4.1-models.jar")
@@ -26,3 +31,27 @@ sd = StanfordDependencies.get_instance(jar_filename="/home/mcapizzi/Github/Seman
 
 #TODO
 #build method for extracting tuples
+
+
+
+##################################################
+import practnlptools
+
+#using SENNA
+annotator = practnlptools.tools.Annotator()
+
+#get all annotations for ONE sentence
+sent = annotator.getAnnotations("I am the walrus.", dep_parse=True)
+
+#batch of sentences
+    #returns list of annotations
+sentBatch = annotator.getBatchAnnotations(["Dogs eat cats.", "Cats eat dogs."], dep_parse=True)
+
+#access just dep_parse
+dep = sent["dep_parse"]
+
+#access semantic role labeling
+srl = sent["srl"]
+
+
+
