e3e64f90d3ec0a87f0ce2faa7adaf89348de84f2
mcapizzi <mcapizzi@email.arizona>
2015-11-30 11:45:31 -0700

    messing with extracting predicates again

---
+++ Testing/testGettingPredicates.py
@@ -0,0 +1,77 @@
+__author__ = 'mcapizzi'
+
+import Data as data
+import Dependencies as dep
+import time
+import pickle
+import multiprocessing
+
+#to see how long it takes to get predicates from wikiSample.txt
+
+#initialize variable for all predicates
+allPreds = []
+
+#open file
+f = open("wikiSample.txt")
+#make Data class
+dataClass = data.Data(f)
+#tokenize
+dataClass.sentenceTokenize()
+#clean
+dataClass.makeASCII()
+#make dependencies class
+depClass = dep.Dependencies(dataClass.allSentences)
+
+###############################################################
+#get raw Senna deps
+depClass.getSennaDeps()
+###############################################################
+#clean Senna deps
+depClass.cleanDeps("SENNA")
+#extract predicates
+depClass.extractPredicates("SENNA")
+#add to allPreds
+[allPreds.append(p) for p in depClass.extractedPredicates]
+f.close()
+
+
+################
+#attempt to multiprocess TODO - figure out how to use
+#
+# #method for the whole process
+# def getDeps():
+#     #open file
+#     print ("working on file: " + fileName)
+#     f = open("simpleWikipedia/" + fileName)
+#     #make Data class
+#     dataClass = data.Data(f)
+#     #tokenize
+#     dataClass.sentenceTokenize()
+#     #clean
+#     dataClass.makeASCII()
+#     #make dependencies class
+#     depClass = dep.Dependencies(dataClass.allSentences)
+#     #get raw Senna deps
+#     depClass.getSennaDeps()
+#     #clean Senna deps
+#     depClass.cleanDeps("SENNA")
+#     #extract predicates
+#     depClass.extractPredicates("SENNA")
+#
+#     # return depClass.extractedPredicates
+#     [allPreds.append(pred) for pred in depClass.extractedPredicates]
+#
+# pool = multiprocessing.Pool(processes=4)
+#
+# #set up processes - one for each file
+# [pool.apply(wholeProcess, args=(z,)) for z in os.listdir("simpleWikipedia")]
+#
+
+############################################
+
+#pickle
+f = open("Predicates/Test-extracted-" + time.strftime("%m_%d") + ".pickle", "wb")
+
+pickle.dump(allPreds, f)
+
+f.close()
