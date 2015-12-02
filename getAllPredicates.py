bb32227310c2c5f7e06975f5341393bcc14ddfa8
mcapizzi <mcapizzi@email.arizona>
2015-12-01 20:25:28 -0700

    just batches of 200

--- getAllPredicates.py
+++ getAllPredicates.py
@@ -14,7 +14,7 @@ allPreds = []
 
 
 #iterate through wiki files
-for file in [os.listdir("simpleWikipedia")[0]]:       #TODO if works, remove [0] so that it'll run for all files
+for file in os.listdir("simpleWikipedia"):
     print ("handling file " + file)
     #open file
     f = open("simpleWikipedia/" + file)
@@ -31,37 +31,17 @@ for file in [os.listdir("simpleWikipedia")[0]]:       #TODO if works, remove [0]
     print "building dependency class"
     depClass = dep.Dependencies(dataClass.allSentences, 200)
 
-    # print "setting up queue"
-    #set up queue
-    # q = multiprocessing.Queue()
+    for batch in range(len(depClass.sentences)):
+        raw = depClass.getSennaDeps(depClass.sentences[batch])
+        print raw
+        clean = depClass.cleanDeps("SENNA", raw)
+        print clean
+        preds = depClass.extractPredicates("SENNA", clean)
+        [allPreds.append(p) for p in preds]
 
-    #define function to multiprocess
-    def multiProcess(sentenceBatch):
-        #get dependencies
-        rawBatch = depClass.getSennaDeps(sentenceBatch)
-        #debugging
-            #print first dependency
-        print rawBatch[0]
-        #cleaning dependencies
-        cleanBatch = depClass.cleanDeps("SENNA", rawBatch)
-        #extracting predicates
-        extractedPreds = depClass.extractPredicates("SENNA", cleanBatch)
-        #debugging
-            #print all predicates
-        for i in range(len(extractedPreds)):
-            print extractedPreds[i]
+    print "finished with file %s" %file
 
-        return extractedPreds
 
-    print "initializing processes"
-    pool = multiprocessing.Pool(processes=4)
-    print "starting"
-    results = [pool.apply_async(multiProcess, args=(depClass.sentences[z],)) for z in range(len(depClass.sentences))]
-    retrievedResults = [p.get() for p in results]
-    #add to allPreds
-    [allPreds.append(pred) for pred in retrievedResults]
-
-print allPreds
 #pickle
 f = open("Predicates/extracted-" + time.strftime("%m_%d") + ".pickle", "wb")
 
