36b7d45aa4bcc682b6c8182e43c363bc841b2efd
mcapizzi <mcapizzi@email.arizona>
2015-12-01 20:31:01 -0700

    just batches of 200

--- Dependencies.py
+++ Dependencies.py
@@ -148,8 +148,14 @@ def cleanSENNADep(rawDep):
     regex = r'(\w+)\((\w+)\.?-\d+, (\w+)\.?-\d+\)'     #\.? because some SENNA dependenices attach the period
     for token in rawDep:
         capture = re.search(regex, token)
-        word = capture.group(3)
-        relation = capture.group(1)
+        if capture.group(3):
+            word = capture.group(3)
+        else:
+            word = None
+        if capture.group(1):
+            relation = capture.group(1)
+        else:
+            relation = None
         if capture.group(2) == "ROOT":
             head = None             #TODO what to do if token is head of sentence?
         else:
