d2d71f809a3666acba87aea6dd6a79279c2fa015
mcapizzi <mcapizzi@email.arizona>
2015-12-08 16:34:34 -0700

    built method to test cleaning gigaword in research server

--- Utils/cleanGigaDep.py
+++ Utils/cleanGigaDep.py
@@ -13,14 +13,19 @@ depClass = d.Dependencies([])
 for line in f:
     #clean the dependency
     s = line.split("__")
+    print(s)
     #add to raw depdency data structure
     depClass.sennaRawDependencies.append((s[2], s[1], s[0]))
 
 f.close()
 
+print("cleaning dependencies")
 #extract the necessary ones
 depClass.sennaCleanDependencies = depClass.extractedPredicates("SENNA", depClass.sennaRawDependencies)
 
+print("pickling")
 f = open("Predicates/gigaPreds-" + sys.argv[1] + ".pickle")
 pickle.dump(depClass.sennaRawDependencies, f)
 f.close()
+
+print("finished")
