122dc91cfdcab4fb66742c14a1467e2be4628897
mcapizzi <mcapizzi@email.arizona>
2015-12-17 20:03:06 -0700

    updated getAllPredicatesGiga for after first stuck

--- Utils/getAllPredicatesGiga.py
+++ Utils/getAllPredicatesGiga.py
@@ -10,20 +10,24 @@ import itertools
 #main method to get all predicates from gigaword files
 
 #initialize variable for all predicates
-allPreds = []
+# allPreds = []
 
-finished = []
-stuck = []
-skip = finished + stuck
+d3StuckFile = ["200104"]
+d3FinishedFile = ["199802", "200711", "200604", "200610", "199512", "200003", "200109", "200007", "200005", "200208", "200303", "201004", "199811", "200806", "200106", "200606", "199508", "200406", "200403", "200402", "199912", "201003", "200212", "200210", "200011", "201009"]
+skipd3 = d3StuckFile + d3FinishedFile
+d3ToSkip = map(lambda x: "gigaword_eng_5_d3_xin_eng_" + x, skipd3)
+
+finishedDisk = []
+toSkip = d3ToSkip
 
 allDir = "/work/mcapizzi/Github/Content/src/main/resources/gigaword/"
 allFiles = os.listdir(allDir)
-for disk in allFiles:
+for disk in itertools.ifilter(lambda x: x not in finishedDisk, allFiles):
     print disk
     for folder in os.listdir(allDir + disk + "/data/"):
         print folder
         for fileF in os.listdir(allDir + disk + "/data/" + folder):
-            if fileF not in skip:
+            if fileF not in toSkip:
                 print "handling " + fileF
                 #open file
                 f = open(allDir + disk + "/data/" + folder + "/" + fileF)
@@ -41,18 +45,18 @@ for disk in allFiles:
                 depClass = dep.Dependencies(dataClass.allSentences)
 
                 for batch in range(len(depClass.sentences)):
-                    pf = open("Predicates/Gigaword/" + disk + "_" + fileF + "-" + str(batch+1) + ".pickle", "wb")
+                    pf = open("Predicates/Gigaword/attempt02/" + disk + "_" + fileF + "-" + str(batch+1) + ".pickle", "wb")
                     raw = depClass.getSennaDeps(depClass.sentences[batch])
                     clean = depClass.cleanDeps("SENNA", raw)
                     preds = depClass.extractPredicates("SENNA", clean)
-                    [allPreds.append(p) for p in preds]
+                    # [allPreds.append(p) for p in preds]
                     print("pickling batch %s of file %s" %(str(batch+1), fileF))
                     pickle.dump(preds, pf)
                     pf.close()
                     print "pickled batch %s of file %s" %(str(batch + 1), fileF)
 
-#if it gets through everthing without an error
-f2 = open("Predicates/Gigaword/ALL" + ".pickle", "wb")
-pickle.dump(allPreds, f2)
-f2.close()
+# #if it gets through everthing without an error
+# f2 = open("Predicates/Gigaword/ALL" + ".pickle", "wb")
+# pickle.dump(allPreds, f2)
+# f2.close()
 
